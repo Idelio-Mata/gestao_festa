32aee22c23d7dc94b28d81c2c6f3b58f1fb6a63d
idelio.mata <idelioteofilomata@gmail.com>
2026-04-02 15:22:36 +0200

    fix(convidado): Corige a visualizacao das tabelas

--- src/main/resources/import.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO convidado (id, nome, quantidade_acompanhantes)
-VALUES
-    (1, 'Pedro', 2),
-    (2, 'Maria', 3),
-    (3, 'Ricardo', 1),
-    (4, 'João Silva', 6),
-    (5, 'Maria Santos', 0);;
