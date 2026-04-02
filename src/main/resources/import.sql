b5ecfe589f54435e6f7592b28321c23e082bed6e
idelio.mata <idelioteofilomata@gmail.com>
2026-04-02 11:09:35 +0200

    chore(database): adiciona dados de teste em import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO convidado (id, nome, quantidade_acompanhantes)
+VALUES
+    (1, 'Pedro', 2),
+    (2, 'Maria', 3),
+    (3, 'Ricardo', 1),
+    (4, 'João Silva', 6),
+    (5, 'Maria Santos', 0);;
